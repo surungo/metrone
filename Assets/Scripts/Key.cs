893ca48b050279eeab5d97b0bc68d50b2981cdef
Surungo <surungo@gmail.com>
2020-02-29 16:28:15 -0300

    creaded keys and keydrops

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Key : ScriptableObject
+{
+    public int itemID;
+    public string keyName;
+    public string description;
+    public Sprite image;
+    public string message;
+
+}
---
+++ Assets/Scripts/KeyDrop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDrop : MonoBehaviour
+{
+    public Key key;
+
+    private SpriteRenderer sprite;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.sprite = key.image;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Player player = other.GetComponent<Player>();
+        if (player != null)
+        {
+           // player.AddKey(key);
+            Destroy(gameObject);
+        }
+    }
+}
